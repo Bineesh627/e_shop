dd9d98a4c60d0032feda5cd06a7908c424a0dc00
Bineesh S <timebusiness17@gmail.com>
2025-11-20 18:27:00 +0530

    Removed Unusual files and images

--- runserver.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import subprocess
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "e_shop.settings")
-    subprocess.call([sys.executable, "manage.py", "runserver", "0.0.0.0:8000"])
